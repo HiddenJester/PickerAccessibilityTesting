8a6636bd823962a3b9b0fffa19de9c11a73fd8d7
Timothy Sanders <tsanders@hiddenjester.com>
2020-10-01 15:52:24 -0700

    Initial Commit

---
+++ PickerAccessibilityTesting/PickerAccessibilityTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerAccessibilityTestingApp.swift
+//  PickerAccessibilityTesting
+//
+//  Created by Timothy Sanders on 2020-10-01.
+//
+
+import SwiftUI
+
+@main
+struct PickerAccessibilityTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
